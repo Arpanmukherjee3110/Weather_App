970a83623358cc1b77f1a30eafd6d4e9351607ee
Arpan Mukherjee <119566371+Arpanmukherjee3110@users.noreply.github.com>
2023-11-15 23:14:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,19 +39,19 @@ function success(data) {
   
       if (request.status === 200){
         var data = JSON.parse(request.responseText);
-        try{
+        if (data.results[0].components.city === undefined){
+          document.getElementById("location").innerHTML = "Your Location: "+data.results[0].components.county;
+        }
+        else{
           document.getElementById("location").innerHTML = "Your Location: "+data.results[0].components.city;
         }
-        catch{
-        document.getElementById("location").innerHTML = "Your Location: "+data.results[0].components.county;
-      };
         document.getElementById("licon").src="licon-removebg-preview.png";
-        try{
-          document.getElementById("slocate").innerHTML = data.results[0].components.city.toUpperCase();
+        if (data.results[0].components.city === undefined){
+          document.getElementById("slocate").innerHTML = data.results[0].components.county;
         }
-        catch{
-        document.getElementById("slocate").innerHTML = data.results[0].components.county.toUpperCase();
-      };
+        else{
+          document.getElementById("slocate").innerHTML = data.results[0].components.city;
+        }
         var req = new XMLHttpRequest();
         var st="https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=66334f31881496924b80ada017081aa2";
            req.open("GET", st)
